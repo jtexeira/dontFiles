51f849faafce828bc3f2fe77a1d815f2b61e7921
hitler <a85504@alunos.uminho.pt>
2019-11-14 16:56:55 +0000

    Backup

--- bash_alias.sh
+++ bash_alias.sh
@@ -2,4 +2,4 @@ alias ls='ls --color=auto'
 alias v='nvim -u $DOTFILES/vimrc'
 alias cls=clear
 alias dontup='sh $DOTFILES/updatefiles'
-alias ukkb="$(sed 1q .xinitrc | sed 's/pt /gb /g')"
+alias sudo='sudo '
